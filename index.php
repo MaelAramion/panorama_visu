bb18e8bfe26ce2e15970f89d160e45c7beb20ee3
Maël Aramion <mael.aramion@gmail.com>
2022-07-26 19:54:08 +1100

    Connexion entre les groupes

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+
+if(isset($_GET['groupe'])){
+    $groupe = $_GET['groupe'];
+    if(isset($_GET['panorama_depart'])){
+        $panorama_depart = $_GET['panorama_depart'];
+        switch ($groupe) {
+            case 1:
+                Header('Location: groupe1?panorama_depart='.$panorama_depart);
+                break;
+            case 2:
+                Header('Location: groupe2?panorama_depart='.$panorama_depart);
+                break;
+            case 3:
+                Header('Location: groupe3?panorama_depart='.$panorama_depart);
+                break;
+        }
+    }else{
+        switch ($groupe) {
+            case 1:
+                Header('Location: groupe1');
+                break;
+            case 2:
+                Header('Location: groupe2');
+                break;
+            case 3:
+                Header('Location: groupe3');
+                break;
+        }
+    }
+
+} else{
+    Header('Location: groupe3');
+}
+
+
